f1af7eaf2329dcb2a21f5443ccf7befc082fab88
Fede2265 <113243840+Fede2265@users.noreply.github.com>
2022-10-10 15:55:17 +0200

    Add files via upload

---
+++ assets/JS/index.js
@@ -0,0 +1,7 @@
+alert('Usa il pop up per dare dei messaggi');
+window.alert('Sto studiando JS');
+window.alert('Non disturbare');
+window.alert('Online');
+
+
+
---
+++ assets/JS/naming.js
@@ -0,0 +1,27 @@
+var pet_preferito = ('gatto');
+console.log(pet_preferito);
+
+pet=('gatto');
+var pet=('cane');
+var petPreferito = ('criceto');
+var PetPreferito = ('coniglio');
+
+var $pet = ('giraffa');
+console.log($pet);
+var _pet = ('leone');
+console.log(_pet);
+var _pet2 = 'pantera';
+document.write(_pet2);
+
+var dodici = 12;
+
+var gol_fatti=12;
+var gol_subiti=10;
+
+console.log (gol_fatti-gol_subiti);
+
+var valore=11;
+
+console.log(valore-7);
+
+document.getElementById('titolo').innerHTML='sottotitolo'
